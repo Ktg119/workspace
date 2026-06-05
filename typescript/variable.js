d4f43b4d8f058b1332dd44e75fc0687a972b3df6
Ktg119 <ktgwizard@gmail.com>
2026-06-05 07:12:58 +0000

    typescript

---
+++ typescript/variable.js
@@ -0,0 +1,5 @@
+let greetings = "Hello Tharun";
+let mynum = 6;
+greetings.toLowerCase();
+console.log(greetings);
+export {};
---
+++ typescript/variable.ts
@@ -0,0 +1,12 @@
+let greetings: string ="Hello Tharun";
+
+greetings.toLowerCase();
+console.log(greetings);
+
+//number
+let userID:number =334455;
+
+//boolean
+let isLoggedIn:boolean=false;
+
+export{}
